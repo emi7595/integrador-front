272a5e71c8fad5ad65b9ce12200dcce18af2234d
KarenNR <74948680+KarenNR@users.noreply.github.com>
2023-02-25 19:54:31 -0600

    Basic design for QR scanning component

--- front-registro-maestros-udem/src/App.js
+++ front-registro-maestros-udem/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginForm from './components/login/LoginForm';
-import QRCode from './components/qrcode/QRCode';
+import ScanQRCode from './components/qr/ScanQRCode';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/qr" element={<QRCode />} />
+        <Route path="/qr" element={<ScanQRCode />} />
       </Routes>
     </>
   );
