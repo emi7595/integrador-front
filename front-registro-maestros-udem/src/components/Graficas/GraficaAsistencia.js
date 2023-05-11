a379739baeeab4a90ec31a0360519c9d7d082312
KarenNR <74948680+KarenNR@users.noreply.github.com>
2023-05-10 19:03:54 -0600

    changed tab space

--- front-registro-maestros-udem/src/components/Graficas/GraficaAsistencia.js
+++ front-registro-maestros-udem/src/components/Graficas/GraficaAsistencia.js
@@ -1,42 +1,43 @@
 import React, { PureComponent } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
-function data2 (props) {
-  return ([
-  { name: 'Group A', value: props.asistencia },
-  { name: 'Group B', value: props.retraso },
-  { name: 'Group C', value: props.salidaPrevia },
-  { name: 'Group D', value: props.retrasoSalida },
-  { name: 'Group E', value: props.falta },
-])};
+function data2(props) {
+    return ([
+        { name: 'Group A', value: props.asistencia },
+        { name: 'Group B', value: props.retraso },
+        { name: 'Group C', value: props.salidaPrevia },
+        { name: 'Group D', value: props.retrasoSalida },
+        { name: 'Group E', value: props.falta },
+    ])
+};
 
-const COLORS = ['#adad5c', '#946724', '#c8ddbb', '#f7deb6','#f1c57e'];
+const COLORS = ['#adad5c', '#946724', '#c8ddbb', '#f7deb6', '#f1c57e'];
 // asistencia, retrasoInicial, salidaPrevia, retrasoSalida, falta
 
 class GraficaClases extends PureComponent {
 
-  render() {
-    return (
-      <ResponsiveContainer width="20%" height="100%">
-        <PieChart width={600} height={600}>
-          <Pie
-            data={data2(this.props)}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            // label={renderCustomizedLabel}
-            outerRadius={120}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data2(this.props).map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    );
-  }
+    render() {
+        return (
+            <ResponsiveContainer width="20%" height="100%">
+                <PieChart width={600} height={600}>
+                    <Pie
+                        data={data2(this.props)}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        // label={renderCustomizedLabel}
+                        outerRadius={120}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data2(this.props).map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            </ResponsiveContainer>
+        );
+    }
 }
 
 export default GraficaClases;
--- front-registro-maestros-udem/src/components/Graficas/GraficaAsistenciaInformativo.js
+++ front-registro-maestros-udem/src/components/Graficas/GraficaAsistenciaInformativo.js
@@ -1,45 +1,46 @@
 import React, { PureComponent } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
-function data2 (props) {
-  return ([
-  { name: 'Group A', value: props.aviso },
-  { name: 'Group B', value: props.unidadExterna },
-  { name: 'Group C', value: props.reposicion },
-  { name: 'Group D', value: props.adelanto },
-  { name: 'Group E', value: props.autorizacion },
-  { name: 'Group F', value: props.claseRepuesta },
-])};
+function data2(props) {
+    return ([
+        { name: 'Group A', value: props.aviso },
+        { name: 'Group B', value: props.unidadExterna },
+        { name: 'Group C', value: props.reposicion },
+        { name: 'Group D', value: props.adelanto },
+        { name: 'Group E', value: props.autorizacion },
+        { name: 'Group F', value: props.claseRepuesta },
+    ])
+};
 
-const COLORS = ['#250025', '#2e3350', '#5974bc', '#8c93c7','#c6c1e1','#b5cef7'];
+const COLORS = ['#250025', '#2e3350', '#5974bc', '#8c93c7', '#c6c1e1', '#b5cef7'];
 //const COLORS = ['#250025', '#b5cef7', '#5974bc', '#2e3350','#c6c1e1','#8c93c7'];
 // asistencia, retrasoInicial, salidaPrevia, retrasoSalida, falta
 // aviso, uniExt, reposicion, adelanto, autorizacion, claseRepuesta
 
 class GraficaAsistenciaInformativo extends PureComponent {
 
-  render() {
-    return (
-      <ResponsiveContainer width="20%" height="100%">
-        <PieChart width={600} height={600}>
-          <Pie
-            data={data2(this.props)}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            // label={renderCustomizedLabel}
-            outerRadius={120}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data2(this.props).map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    );
-  }
+    render() {
+        return (
+            <ResponsiveContainer width="20%" height="100%">
+                <PieChart width={600} height={600}>
+                    <Pie
+                        data={data2(this.props)}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        // label={renderCustomizedLabel}
+                        outerRadius={120}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data2(this.props).map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            </ResponsiveContainer>
+        );
+    }
 }
 
 export default GraficaAsistenciaInformativo;
